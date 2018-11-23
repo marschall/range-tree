570393d9c4aa2d28cef37f41cae1431a2f858372
Philippe Marschall <philippe.marschall@gmail.com>
2018-11-23 09:45:06 +0100

    second jab at read write lock

--- src/test/java/com/github/marschall/rangetree/RWLockRangeTree.java
+++ src/test/java/com/github/marschall/rangetree/RWLockRangeTree.java
@@ -7,7 +7,7 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Function;
 
-public final class RWLockRangeTree <K extends Comparable<? super K>, V> implements RangeMap<K, V> {
+public final class RWLockRangeTree<K extends Comparable<? super K>, V> implements RangeMap<K, V> {
 
   private final RangeMap<K, V> delegate;
 
@@ -43,15 +43,18 @@ public final class RWLockRangeTree <K extends Comparable<? super K>, V> implemen
 
   @Override
   public V computeIfAbsent(K key, Function<? super K, Entry<Range<? extends K>, ? extends V>> mappingFunction) {
-    // FIXME
-    Lock writeLock = this.lock.writeLock();
-    writeLock.lock();
+    Lock readLock = this.lock.readLock();
+    LazyWriteLock writeLock = new LazyWriteLock(this.lock);
+    readLock.lock();
     try {
-      this.delegate.computeIfAbsent(key, mappingFunction);
+      return this.delegate.computeIfAbsent(key, lambdaKey -> {
+        writeLock.lock();
+        return mappingFunction.apply(lambdaKey);
+      });
     } finally {
       writeLock.unlock();
+      readLock.unlock();
     }
-    throw new UnsupportedOperationException();
   }
 
   @Override
@@ -65,5 +68,49 @@ public final class RWLockRangeTree <K extends Comparable<? super K>, V> implemen
     }
   }
 
+  @Override
+  public V putIfAbsent(K low, K high, V value) {
+    // the implementation here is tricky
+    // 1. we want to try to read with a read lock
+    // 2. if do not find anything we have to abort, acquire a write
+    // lock, try again and possibly write. We can not upgrade the lock
+    // because:
+    //  - Java APIs do not support it
+    //  - two updates may race the the same write lock on the same key,
+    //    the loser has to check again before doing the write
+    throw new UnsupportedOperationException();
+//    Lock writeLock = this.lock.writeLock();
+//    writeLock.lock();
+//    try {
+//      return this.delegate.putIfAbsent(low, high, value);
+//    } finally {
+//      writeLock.unlock();
+//    }
+  }
+
+  static final class LazyWriteLock {
+
+    private final ReadWriteLock lock;
+    private Lock writeLock;
+
+    LazyWriteLock(ReadWriteLock lock) {
+      this.lock = lock;
+    }
+
+    void lock() {
+      if (this.writeLock != null) {
+        throw new IllegalStateException();
+      }
+      this.writeLock = this.lock.writeLock();
+      this.writeLock.lock();
+    }
+
+    void unlock() {
+      if (this.writeLock != null) {
+        this.writeLock.lock();
+      }
+    }
+
+  }
 
 }
---
+++ src/test/java/com/github/marschall/rangetree/RWLockRangeTreeTest.java
@@ -0,0 +1,48 @@
+package com.github.marschall.rangetree;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.AbstractMap.SimpleEntry;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class RWLockRangeTreeTest {
+
+  private static final String VALUE = "Range 1";
+
+  private RangeMap<Integer, String> tree;
+
+  @BeforeEach
+  void setUp() {
+    this.tree = new RWLockRangeTree<Integer, String>(new LLRBRangeTree<>());
+    this.tree.put(10, 19, VALUE);
+  }
+
+  @Test
+  void clear() {
+    this.tree.clear();
+    assertNull(this.tree.get(15));
+  }
+
+  @Test
+  void get() {
+    assertEquals(VALUE, this.tree.get(19));
+  }
+
+  @Test
+  @Disabled("FIXME")
+  void computeIfAbsent() {
+    assertEquals(VALUE, this.tree.computeIfAbsent(19, key -> null));
+    assertEquals("Range 2", this.tree.computeIfAbsent(20, key -> new SimpleEntry<>(new Range<>(20, 29), "Range 2")));
+  }
+
+  @Test
+  void put() {
+    this.tree.put(20, 29, "Range 2");
+    assertEquals("Range 2", this.tree.get(21));
+  }
+
+}
